1ad317651a13724dc972c4a75f55709de79e7d98
ruchi-cme <ruchi.cmexpertise@gmail.com>
2022-11-02 14:46:30 +0530

    change js

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div>
     <script src="{{asset('js/front')}}/bootstrap.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
+    <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js" type="text/javascript"></script>
     <script src="{{asset('js/front')}}/owl.carousel.js"></script>
     <script src="{{asset('js/front')}}/jquery.waypoints.min.js"></script>
     <script src="{{asset('js/front')}}/inview.min.js"></script>
